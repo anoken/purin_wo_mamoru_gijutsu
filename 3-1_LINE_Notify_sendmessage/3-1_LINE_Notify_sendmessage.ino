ef34632886f5cdbc017ff100bb18426e808811c3
anoken <31365453+anoken@users.noreply.github.com>
2019-05-12 23:21:57 +0900

    Update 3-1_LINE_Notify_sendmessage.ino

--- 3-1_LINE_Notify_sendmessage/3-1_LINE_Notify_sendmessage.ino
+++ 3-1_LINE_Notify_sendmessage/3-1_LINE_Notify_sendmessage.ino
@@ -1,13 +1,9 @@
 //Copyright (c) 2019 aNo研 プリンを守る技術 
 //https://github.com/anoken/purin_wo_mamoru_gijutsu/
-//
-
-
 
 #include <ssl_client.h>
 #include <WiFiClientSecure.h>
 #include <M5Stack.h>
-/* wifi環境設定 */
 
 /* LINENofifyの設定 */
 const char* host = "notify-api.line.me";
@@ -18,17 +14,20 @@ void setup() {
   wifi_connect();		//Wifiに接続する
   send_line_alert();	//LINEへの送信
 }
+
 void loop() {
   M5.update();		//M5Stackの内部処理を更新
+  delay(500);
 }
  /* Wifiに接続する */
 void wifi_connect() {	
   const char* ssid = "your_wifi_ssid";
-const char* password = "your_wifi_pass";
+  const char* password = "your_wifi_pass";
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)  delay(500);
   Serial.println(WiFi.localIP());
 }
+
  /* LINEへの送信 */
 void send_line_alert() {
   const char* message1 = "プリンが取られました！！！";
@@ -36,10 +35,10 @@ void send_line_alert() {
   if (!client.connect(host, 443)) {
     return;
   }
-/* LINE NotifyのサーバへHTTPS通信でメッセージをPOSTする */
+  /* LINE NotifyのサーバへHTTPS通信でメッセージをPOSTする */
   String query = "message=" + String(message1);
   
-    String request = String("") +
+  String request = String("") +
                    "POST /api/notify HTTP/1.1\r\n" +
                    "Host: " + host + "\r\n" +
                    "Authorization: Bearer " + token + "\r\n" +
